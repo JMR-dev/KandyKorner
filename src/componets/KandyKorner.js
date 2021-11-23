5eb6f6d842337fca910f82fb0f917165f0087858
bytesparallel <jason.ross841@gmail.com>
2021-11-23 11:28:00 -0600

    Got info to start displaying on the browser

--- src/componets/KandyKorner.js
+++ src/componets/KandyKorner.js
@@ -1,4 +1,11 @@
+import { LocationsList } from "./locations/Locations"
 
 export const KandyKorner = () => {
-    
+    return(
+    <>
+    <h1>Kandy Korner</h1>
+    <h2>Locations</h2>
+    <LocationsList />
+    </>
+)
 }
